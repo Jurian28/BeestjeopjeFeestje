f94fc0528ac1b9f3b5f031c48aa864fe40a0c24c
Jurian28 <jan.shertegel@gmail.com>
2025-04-04 17:29:23 +0200

    eerste test gemaakt

--- BeestjeOpJeFeestjeDb/Models/Animal.cs
+++ BeestjeOpJeFeestjeDb/Models/Animal.cs
@@ -1,10 +1,12 @@
-﻿namespace BeestjeOpJeFeestjeDb.Models {
+﻿using Microsoft.EntityFrameworkCore.Storage;
+
+namespace BeestjeOpJeFeestjeDb.Models {
     public class Animal {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Type { get; set; }
-        public decimal Price { get; set; }
-        public string ImageUrl { get; set; }
+        public string Type { get; set; } = "";
+        public decimal Price { get; set; } = 0;
+        public string ImageUrl { get; set; } = "";
 
         public ICollection<BookingAnimal> BookingAnimals { get; set; } = [];
     }
---
+++ BeestjeOpJeFeestjeTest/BoerderijControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using BeestjeOpJeFeestje.Controllers;
+using BeestjeOpJeFeestjeDb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Moq.EntityFrameworkCore;
+
+namespace BeestjeOpJeFeestjeTest {
+    [TestClass]
+    public class BoerderijControllerTest {
+
+        [TestMethod]
+        public void Index_ShouldReturn_ViewResult_WithAnimals_WithoutMocks() {
+            var options = new DbContextOptionsBuilder<MyContext>()
+                .UseInMemoryDatabase(databaseName: "AnimalTestDb")
+                .Options;
+
+            using (var context = new MyContext(options)) {
+                context.Animals.Add(new Animal { Name = "Lion" });
+                context.Animals.Add(new Animal { Name = "Paard" });
+                context.SaveChanges();
+
+                var controller = new BoerderijController(context);
+
+                var result = controller.Index();
+
+                Assert.IsInstanceOfType(result, typeof(ViewResult));
+
+                var viewResult = result as ViewResult;
+                Assert.IsNotNull(viewResult);
+
+                var model = viewResult.Model as IEnumerable<Animal>;
+                Assert.IsNotNull(model);
+                Assert.AreEqual(2, model.Count());
+                Assert.AreEqual("Lion", model.First().Name);
+            }
+        }
+    }
+}
